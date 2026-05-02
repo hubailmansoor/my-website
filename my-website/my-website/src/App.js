3c8f0872d74872e1950507878627b8e7c5eef063
Hubail <hubailmansoor@gmail.com>
2026-05-03 01:23:01 +0530

    first commit

---
+++ my-website/my-website/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, I'm Jack 👋</h1>
+      <p>Welcome to my website. This is React site!</p>
+
+      <div className="card">
+        <h2>About Me</h2>
+        <p>Doing nothing hits.</p>
+      </div>
+
+      <div className="card">
+        <h2>Where to find me</h2>
+        <p>You can reach me at somewhere</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
