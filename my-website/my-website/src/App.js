bae6a14a4f0f01c4b65022424c5f1c185e231032
Hubail <hubailmansoor@gmail.com>
2026-05-04 13:41:38 +0530

    add email

--- my-website/my-website/src/App.js
+++ my-website/my-website/src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1>Hello, I'm Jalack 👋</h1>
-      <p>Welcome to my website. This is React site!</p>
+      <h1>Hello, I'm Jack 👋</h1>
+      <p>Welcome to my website. This is my first React site!</p>
 
       <div className="card">
         <h2>About Me</h2>
-        <p>Doing nothing hits.</p>
+        <p>Write something about yourself here.</p>
       </div>
 
       <div className="card">
         <h2>Where to find me</h2>
-        <p>You can reach me at somewher</p>
+        <p>You can reach me at: hubailmansoor@gmail.com</p>
       </div>
     </div>
   );
